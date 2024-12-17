67669702df21e071909dc4697cf5e0c0e6aee245
STYCA <styca42@proton.me>
2024-12-17 10:44:08 +0100

    Update final.py

--- 17/final.py
+++ 17/final.py
@@ -1,33 +1,9 @@
 import re
-from tqdm import tqdm
 
-
-MODE = 0
-NUMBER = 0
 
 def read_file(file):
     with open(file, 'r') as file:
-        if MODE == 0:
-            # return list of strings
-            return [line.strip() for line in file]
-        elif MODE == 1:
-            # return list of tuples of strings
-            return [tuple(line.strip().split()) for line in file]
-        elif MODE == 2:
-            # return list of integers
-            return [int(line) for line in file]
-        elif MODE == 3:
-            # return list of tuples of integers
-            return [tuple(map(int, line.strip().split())) for line in file]
-        elif MODE == 4:
-            # return list of parallel lists of integers
-            return [[int(line.split()[i]) for line in file] for i in range(NUMBER)]
-        elif MODE == 5:
-            # return list of parallel lists of strings
-            return [[line.split()[i] for line in file] for i in range(NUMBER)]
-        elif MODE == 6:
-            #return all lines as a single string
-            return file.read().strip()
+        return file.readlines()
         
 def decrypt(l):
     pattern = r"Register\s[A-C]:\s(\d+)"
@@ -106,7 +82,6 @@ def part2(l):
     registers, program = decrypt(l)
     A = find(len(program)-1, 0, program)
     return A
-    
 
 
 if __name__ == "__main__":
